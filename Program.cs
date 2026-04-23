afbc2e1bb0fceb7cf792a91b0ad36057cbaadf7b
Pavan Varma <pavanvarma058@gmail.com>
2026-04-23 11:29:15 +0100

    Car class completed

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace SDD_Lab_Week_3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Car car1 = new Car("Toyota", 2020, 10, 100, 30, "Charlie");
+        car1.display();
+        Console.WriteLine("Petrol after filling 20 gallons: " + car1.fillPetrol(20));
+        Console.WriteLine("Petrol after filling 20 gallons: " + car1.fillPetrol(20));
+        Console.WriteLine("Distance travelled: " + car1.drive(150));
+        Console.WriteLine(car1.changeOwner("Alan"));
+        car1.display();
+    }
+}
---
+++ car.cs
@@ -0,0 +1,74 @@
+class Car
+{
+    private string brand;
+    private int year;
+    private long petrol;
+    private double tankSize;
+    private double mpg;
+    private string owner;
+
+    public Car()
+    {
+        brand = "";
+        year = 0;
+        petrol = 0;
+        tankSize = 100;
+        mpg = 0;
+        owner = "";
+    }
+
+    public Car(string brand, int year, long petrol, double tankSize, double mpg, string owner)
+    {
+        this.brand = brand;
+        this.year = year;
+        this.petrol = petrol;
+        this.tankSize = tankSize;
+        this.mpg = mpg;
+        this.owner = owner;
+    }
+
+    public void display()
+    {
+        Console.WriteLine("Car Details:");
+        Console.WriteLine("Brand: " + brand);
+        Console.WriteLine("Year: " + year);
+        Console.WriteLine("Owner: " + owner);    
+    }
+
+    public double fillPetrol(double gallons)
+    {
+        double space = tankSize - petrol;
+        if(gallons <= space)
+        {
+            petrol += (long)gallons;
+            return petrol;
+        }
+        else
+        {
+            petrol = (long)tankSize;
+            return petrol;
+        }
+    }
+
+    public double drive(double distance)
+    {
+        double expectedDistance = petrol * mpg;
+        if(distance <= expectedDistance)
+        {
+            double fuelUsed = distance / mpg;
+            petrol -= (long)fuelUsed;
+            return distance;
+        }
+        else
+        {
+            petrol = 0;
+            return expectedDistance;
+        }
+    }
+
+    public string changeOwner(string newOwner)
+    {
+        owner = newOwner;
+        return owner;
+    }
+}
